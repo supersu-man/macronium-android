5b5b7d82a8bf8b0c621a454419f16cf2c004125b
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-02-01 01:00:20 +0530

    v2.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.supersuman.macronium"
         minSdk = 24
         targetSdk = 34
-        versionCode = 16
-        versionName = "2.3.2"
+        versionCode = 17
+        versionName = "2.3.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
